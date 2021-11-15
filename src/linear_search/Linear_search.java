1f14b865763837463c891dddc5ff1e54ee0844f6
TANIMUL ISLAM <35636662+Tanimul@users.noreply.github.com>
2021-11-16 00:31:04 +0600

    Update Linear_search.java

--- src/linear_search/Linear_search.java
+++ src/linear_search/Linear_search.java
@@ -11,11 +11,15 @@ public class Linear_search {
 
     public static void main(String[] args) {
         int numbers[] = {5, 2, 3, 4, 8, 10, 40, 50};
-        System.out.println("Enter a value!");
+        System.out.println("Enter a value: ");
         int value = getInput.nextInt();
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == value) {
                 System.out.println(value + " is found at index: " + i);
+                break;
+            }
+            if (i == numbers.length - 1) {
+                System.out.println("Element not found!");
             }
         }
     }
